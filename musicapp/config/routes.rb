cec867560e946cf3265df98f0ee47e519527d997
DOV3D <davidovelazquez3@gmail.com>
2021-09-21 17:36:41 -0400

    Add album routes

--- musicapp/config/routes.rb
+++ musicapp/config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
 
   resource :session, only: [:new, :create, :destroy]
  
- resources :bands
+ resources :bands do
+  resources :albums, only: [:new]
+ end
+
+ resources :albums, only: [:create, :edit, :show, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
