3e91f647bab6cb2ba19c3214bc4bb913d5dfc8ba
DOV3D <davidovelazquez3@gmail.com>
2021-09-22 00:05:16 -0400

    Add Rspec

--- musicapp/app/models/user.rb
+++ musicapp/app/models/user.rb
@@ -31,7 +31,7 @@ class User < ApplicationRecord
         SecureRandom::urlsafe_base64
     end
 
-    def reset_session_token
+    def reset_session_token!
         self.session_token = self.class.generate_session_token
         self.save!
         self.session_token
